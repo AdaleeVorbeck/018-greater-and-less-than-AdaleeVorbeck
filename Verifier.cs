21d04edb52e8b0c0d566099a06438b297f9b847a
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-11 18:00:10 +0000

    fdjsklkjsd

--- Verifier.cs
+++ Verifier.cs
@@ -2,4 +2,43 @@
 public class Verifier
 {
     // Add your code here
+    public int OldEnough(int age){
+        if (age > 17) {
+            return "old enough";
+        }
+        return "";
+    }
+
+    public int UnderAge(int age){
+        if (age < 18) {
+            return "under age";
+        }
+        return "";
+    }
+
+    public int LegalSpeed(int speed){
+        if (speed <= 50) {
+            return "legal speed";
+        }
+        return "";
+    }
+
+    public int IsGoldenYears(int age){
+        if (age >= 65) {
+            return "golden years";
+        }
+        return "";
+    }
 }
+
+//should create a method named OldEnough that takes an age and returns the string "old enough" if the age is over 17
+//should return an empty string ("") if an age of 17 is passed to OldEnough
+
+//should create a method named UnderAge that takes an age and returns the string "under age" if the age is under 18
+//should return an empty string ("") if an age of 18 is passed to UnderAge
+//should create a method named LegalSpeed that takes a speed and returns the string "legal speed" if the speed is under 50
+//should return the string "legal speed" if a speed of 50 is passed to LegalSpeed
+//should return an empty string ("") if a speed of 51 is passed to LegalSpeed
+//should create a method named IsGoldenYears that takes an age and returns the string "golden years" if the age is over 65
+//should return the string "golden years" if an age of 65 is passed to IsGoldenYears
+//should return an empty string ("") if an age of 64 is passed to IsGoldenYears
